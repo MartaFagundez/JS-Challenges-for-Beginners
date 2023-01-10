0df312d8a66e84ba760b47840c14dccd8a0e2527
MartaFagundez <martafagundez.dev@gmail.com>
2023-01-10 20:53:03 -0300

    Added and solved findLargestNums(arr){} function (easy level)

--- easy_solved.js
+++ easy_solved.js
@@ -98,3 +98,59 @@ function cardHide(card) {
    let asterisks = card.slice(0, -4).replace(/[0-9]/g, "*");
    return asterisks.concat("", numbers);
 }
+
+
+// 1- Solved using nested for loops
+function findLargestNums(arr) {
+	/*
+    Source: https://edabit.com/challenge/h7LTMAFeNz79rXB2Y
+    Create a function that takes an array of arrays with numbers. 
+    Return a new (single) array with the largest numbers of each.
+    Examples:
+        findLargestNums([[4, 2, 7, 1], [20, 70, 40, 90], [1, 2, 0]]) ➞ [7, 90, 2]
+        findLargestNums([[-34, -54, -74], [-32, -2, -65], [-54, 7, -43]]) ➞ [-34, -2, 7]
+        findLargestNums([[0.4321, 0.7634, 0.652], [1.324, 9.32, 2.5423, 6.4314], [9, 3, 6, 3]]) ➞ [0.7634, 9.32, 9]
+    Notes:
+        Watch out for negative integers (numbers).
+    */
+   let mayor;
+   let newArr = [];
+   for (let i = 0; i < arr.length; i++) {
+        mayor = arr[i][0];
+        for (let j = 1; j < arr[i].length; j++) {
+            if (arr[i][j] > mayor) {
+                mayor = arr[i][j];
+            }
+        }
+        newArr.push(mayor);
+   }
+   return newArr;
+}
+
+
+// 2- Solved using nested forEach loops
+function findLargestNums(arr) {
+	/*
+    Source: https://edabit.com/challenge/h7LTMAFeNz79rXB2Y
+    Create a function that takes an array of arrays with numbers. 
+    Return a new (single) array with the largest numbers of each.
+    Examples:
+        findLargestNums([[4, 2, 7, 1], [20, 70, 40, 90], [1, 2, 0]]) ➞ [7, 90, 2]
+        findLargestNums([[-34, -54, -74], [-32, -2, -65], [-54, 7, -43]]) ➞ [-34, -2, 7]
+        findLargestNums([[0.4321, 0.7634, 0.652], [1.324, 9.32, 2.5423, 6.4314], [9, 3, 6, 3]]) ➞ [0.7634, 9.32, 9]
+    Notes:
+        Watch out for negative integers (numbers).
+    */
+    let mayor;
+    let newArr = [];
+    arr.forEach(element => {
+        mayor = element[0];
+        element.forEach(subElement => {
+            if (subElement > mayor) {
+                mayor = subElement;
+            }
+        });
+        newArr.push(mayor);
+    }); 
+    return newArr;
+}
