b197f2819d6b5f3b4edd9c28610dfdbcacb721d2
MartaFagundez <martafagundez.dev@gmail.com>
2023-01-10 21:31:10 -0300

    easy_solved.js updated

--- easy_solved.js
+++ easy_solved.js
@@ -154,3 +154,47 @@ function findLargestNums(arr) {
     }); 
     return newArr;
 }
+
+
+// 3- Solved using map() method
+function findLargestNums(arr) {
+	/*
+    Source: https://edabit.com/challenge/h7LTMAFeNz79rXB2Y
+    Create a function that takes an array of arrays with numbers. 
+    Return a new (single) array with the largest numbers of each.
+    Examples:
+        findLargestNums([[4, 2, 7, 1], [20, 70, 40, 90], [1, 2, 0]]) ➞ [7, 90, 2]
+        findLargestNums([[-34, -54, -74], [-32, -2, -65], [-54, 7, -43]]) ➞ [-34, -2, 7]
+        findLargestNums([[0.4321, 0.7634, 0.652], [1.324, 9.32, 2.5423, 6.4314], [9, 3, 6, 3]]) ➞ [0.7634, 9.32, 9]
+    Notes:
+        Watch out for negative integers (numbers).
+    */
+    let mayor;
+    let newArr = arr.map(element => {
+        mayor = element[0];
+        element.forEach(subElement => {
+            if (subElement > mayor) {
+                mayor = subElement;
+            }
+        });
+        return mayor;
+    }); 
+    return newArr;
+}
+
+
+// 4- Solved using map() and Math.max() methods with spread syntax
+function findLargestNums(arr) {
+	/*
+    Source: https://edabit.com/challenge/h7LTMAFeNz79rXB2Y
+    Create a function that takes an array of arrays with numbers. 
+    Return a new (single) array with the largest numbers of each.
+    Examples:
+        findLargestNums([[4, 2, 7, 1], [20, 70, 40, 90], [1, 2, 0]]) ➞ [7, 90, 2]
+        findLargestNums([[-34, -54, -74], [-32, -2, -65], [-54, 7, -43]]) ➞ [-34, -2, 7]
+        findLargestNums([[0.4321, 0.7634, 0.652], [1.324, 9.32, 2.5423, 6.4314], [9, 3, 6, 3]]) ➞ [0.7634, 9.32, 9]
+    Notes:
+        Watch out for negative integers (numbers).
+    */
+    return arr.map(element => Math.max(...element));
+}
