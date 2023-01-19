b50f5d174cf2968e5198c9ba74bdc5bc95b1e404
MartaFagundez <martafagundez.dev@gmail.com>
2023-01-18 23:18:44 -0300

    getOnlyEvens(nums) function added (easy level)

--- easy.js
+++ easy.js
@@ -201,3 +201,16 @@ function removeVowels(str) {
         ➞ "Hppy Thnksgvng t ll--vn th htrs nd lsrs!"
     */
 }
+
+
+function getOnlyEvens(nums) {
+	/*
+    Source: https://edabit.com/challenge/6MZx5RqKYkFaogeAQ
+    Given an array of numbers, return an array which contains all the even numbers in the original array, 
+    which also have even indices.
+    Examples:
+        getOnlyEvens([1, 3, 2, 6, 4, 8]) ➞ [2, 4]
+        getOnlyEvens([0, 1, 2, 3, 4]) ➞ [0, 2, 4]
+        getOnlyEvens([1, 2, 3, 4, 5]) ➞ []
+    */
+}
--- easy_solved.js
+++ easy_solved.js
@@ -316,3 +316,17 @@ function removeVowels(str) {
     */
    return str.replace(/[aeiou]/gi, "");
 }
+
+
+function getOnlyEvens(nums) {
+	/*
+    Source: https://edabit.com/challenge/6MZx5RqKYkFaogeAQ
+    Given an array of numbers, return an array which contains all the even numbers in the original array, 
+    which also have even indices.
+    Examples:
+        getOnlyEvens([1, 3, 2, 6, 4, 8]) ➞ [2, 4]
+        getOnlyEvens([0, 1, 2, 3, 4]) ➞ [0, 2, 4]
+        getOnlyEvens([1, 2, 3, 4, 5]) ➞ []
+    */
+   return nums.filter((element, index) => element % 2 === 0 && index % 2 === 0);
+}
