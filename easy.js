95763fcd3054bfc83d9dcdc43783d36122f91530
MartaFagundez <martafagundez.dev@gmail.com>
2023-01-26 21:03:00 -0300

    whichIsLarger(f, g) function added (easy level)

--- easy.js
+++ easy.js
@@ -241,3 +241,21 @@ function amplify(num) {
         A number a is exactly divisible by a number b when the remainder of the division a / b is equal to 0.
     */
 }
+
+
+function whichIsLarger(f, g) {
+	/*
+    Source: https://edabit.com/challenge/o7TwicAHWuMkjbDqQ
+    Your function will be passed two functions, f and g, that don't take any parameters. 
+    Your function has to call them, and return a string which indicates which function returned the larger number.
+        If f returns the larger number, return the string f.
+        If g returns the larger number, return the string g.
+        If the functions return the same number, return the string neither.
+    Examples:
+        whichIsLarger(() => 5, () => 10) ➞ "g"
+        whichIsLarger(() => 25,  () => 25) ➞ "neither"
+        whichIsLarger(() => 505050, () => 5050) ➞ "f"
+    Notes:
+    This exercise is designed as an introduction to higher order functions (functions which use other functions to do their work).
+    */
+}
--- easy_solved.js
+++ easy_solved.js
@@ -384,3 +384,24 @@ function amplify(num) {
    }
    return arr;
 }
+
+
+function whichIsLarger(f, g) {
+	/*
+    Source: https://edabit.com/challenge/o7TwicAHWuMkjbDqQ
+    Your function will be passed two functions, f and g, that don't take any parameters. 
+    Your function has to call them, and return a string which indicates which function returned the larger number.
+        If f returns the larger number, return the string f.
+        If g returns the larger number, return the string g.
+        If the functions return the same number, return the string neither.
+    Examples:
+        whichIsLarger(() => 5, () => 10) ➞ "g"
+        whichIsLarger(() => 25,  () => 25) ➞ "neither"
+        whichIsLarger(() => 505050, () => 5050) ➞ "f"
+    Notes:
+    This exercise is designed as an introduction to higher order functions (functions which use other functions to do their work).
+    */
+   return f() > g() ? "f"
+        : g() > f() ? "g"
+        : "neither";
+}
