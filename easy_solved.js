e6bd43e819f5b741f46f360ef165bf30ca4be8c5
MartaFagundez <martafagundez.dev@gmail.com>
2023-01-28 23:01:16 -0300

    sortDrinkByPrice(drinks) function added (easy level)

--- easy_solved.js
+++ easy_solved.js
@@ -405,3 +405,20 @@ function whichIsLarger(f, g) {
         : g() > f() ? "g"
         : "neither";
 }
+
+
+function sortDrinkByPrice(drinks) {
+	/*
+    Source: https://edabit.com/challenge/nuXdWHAoHv9y38sn7
+    You will be given an array of drinks, with each drink being an object with two properties: name and price. 
+    Create a function that has the drinks array as an argument and return the drinks objects sorted by price in ascending order.
+    Assume that the following array of drink objects needs to be sorted:
+        drinks = [
+        {name: "lemonade", price: 50},
+        {name: "lime", price: 10}
+        ]
+    The output of the sorted drinks object will be:
+        sortDrinkByPrice(drinks) ➞ [{name: "lime", price: 10}, {name: "lemonade", price: 50}]
+    */
+   return drinks.sort((firstItem, secondItem) => firstItem.price - secondItem.price);
+}
